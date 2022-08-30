9d60e0a18f5eedfc8ddfc4e306778e6b2b25da47
204g1a0510 <94513305+204g1a0510@users.noreply.github.com>
2022-08-30 11:09:13 +0530

    G.Amrutha created Sample.c

---
+++ OSLAB/Sample.c
@@ -0,0 +1,7 @@
+/*
+Name of the Candidate:G.Amrutha
+RollNo:204g1a0510
+Title of the experiment:CPU Scheduling Using FCFS Algorithm
+Date of the creation: 30-08-2022
+Date of the execution: 29-08-2022
+*/
