dcc2172f227585ac2e36329a1525de668870e2c5
204G1A0510 <204g1a0510@srit.ac.in>
2022-09-03 18:22:05 +0530

    Amrutha G has completed sample.c

--- OS LAB/Sample1.c
+++ OS LAB/Sample1.c
@@ -1,4 +1,5 @@
-/* Name Of the Candidate: G.Amrutha
+/* 
+   Name Of the Candidate: G.Amrutha
    Roll No: 204G1A0510
    Title of the Experiment: CPU Scheduling Using FCFS Algorithm
    Date of Creation: 30-08-2022
